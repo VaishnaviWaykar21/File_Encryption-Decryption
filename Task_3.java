c94790ed3e19377529e247c787f0ce9ef288713d
Vaishnavi Waykar <119470671+VaishnaviWaykar21@users.noreply.github.com>
2024-10-04 14:16:41 +0530

    Add files via upload

---
+++ Task_3.java
@@ -0,0 +1,69 @@
+//Task: File Encryption/Decryption
+
+package Level_2;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Task_3 {
+	
+    public static String shift(String text, int shift) {
+        StringBuilder result = new StringBuilder();
+
+        for(char ch : text.toCharArray()) {
+            if(Character.isLetter(ch)) {
+                char base = Character.isLowerCase(ch) ? 'a' : 'A';
+                ch = (char) ((ch - base + shift + 26) % 26 + base); 
+            }
+            result.append(ch);
+        }
+        return result.toString();
+    }
+
+    public static void processFile(String inputFile, int shift) throws IOException {
+        BufferedReader read = new BufferedReader(new FileReader(inputFile));
+        
+        String line;
+        while((line = read.readLine()) != null) {
+            String processedLine = shift(line, shift); 
+            System.out.println(processedLine);
+        }
+        
+        read.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter 1 to Encrypt or 2 to Decrypt: ");
+        int choice = sc.nextInt();
+        sc.nextLine(); 
+        
+        System.out.print("Enter the input file path: ");
+        String inputFile = sc.nextLine();
+        
+        int shift = (choice == 1) ? 3 : -3;
+        processFile(inputFile, shift);
+        
+        sc.close();
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//C:\Users\DELL\OneDrive\Desktop\Cognifyz_Technologies\src\Level_2\input.txt
+//C:\Users\DELL\OneDrive\Desktop\Cognifyz_Technologies\src\Level_2\output.txt
